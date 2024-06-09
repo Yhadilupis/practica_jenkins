dd4708e1ecea417ceaff17b811cad3d308f296c3
Yhadilupis <201275@ids.upchiapas.edu.mx>
2024-06-08 23:39:56 -0600

    hello worl prueba

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.json());
 
 
-app.get('/api/v1/Bienvenido', (req, res) => {
-  res.status(200).json({ message: 'Welcome :3!!!' });
+app.get('/api/v1/hello', (req, res) => {
+  res.status(200).json({ message: 'Hello world :3' });
 });
 
 const PORT = 3000;
