9c76d366bef2c28d91860eef04622be412c53049
Yhadilupis <201275@ids.upchiapas.edu.mx>
2024-06-08 23:45:33 -0600

    test

--- tests/integrations/welcome.test.cjs
+++ tests/integrations/welcome.test.cjs
@@ -1,15 +1,15 @@
 const request = require('supertest');
 const app = require('../../app.js');
 
-describe('GET /api/v1/welcome', () => {
+describe('GET /api/v1/hello', () => {
   it('responds with Welcome message', (done) => {
     request(app)
-      .get('/api/v1/welcome')
+      .get('/api/v1/hello')
       .expect('Content-Type', /json/)
       .expect(200)
       .expect((res) => {
         if (!('message' in res.body)) throw new Error("Missing 'message' key");
-        if (res.body.message !== 'Welcome Bitch!!!') throw new Error(`Unexpected message: ${res.body.message}`);
+        if (res.body.message !== 'Hello world :3') throw new Error(`Unexpected message: ${res.body.message}`);
       })
       .end(done);
   });
