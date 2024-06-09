b7fa9c20e53c3bb82ce7fd568a7fdd22676d0682
Yhadilupis <201275@ids.upchiapas.edu.mx>
2024-06-08 23:48:51 -0600

    segundo test

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/api/v1/hello', (req, res) => {
   res.status(200).json({ message: 'Hello world :3' });
 });
 
+app.get('/api/v1/bye', (req, res) => {
+  res.status(200).json({ message: 'bye' });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
